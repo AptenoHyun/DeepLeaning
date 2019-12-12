31642a715fefc27eca366d06d695045938fed994
KwonHyun <aptenodytes@live.co.kr>
2019-12-13 00:00:20 +0900

    Change Matrix to Override

--- DeepLenaing/DeepLenaing/matrix.hpp
+++ DeepLenaing/DeepLenaing/matrix.hpp
@@ -6,8 +6,26 @@ template <typename T, int width, int height>
 class CMatrix : std::array<std::array<T, width>, height>
 {
 public:
-	std::error_code&& dot(std::vector<T> & vInput, std::vector<T>& vOutput)
-	{
-		std::error_code error_code = std::error_category::
+	CMatrix CMatrix::operator*(const CMatrix& cMatrix) override{
+		CMatrix<decltype(this[0][0]), this->size(), cMatrix[0].size()> cDotMatrix;
+		
+		for (int i = 0; i < this->size(); i++)
+		{
+			for (int j = 0; j < cMatrix[0].size(); j++)
+			{
+				decltype(this[0][0]) value = 0;
+				for (int k = 0; k < this[0].size(); k++)
+				{
+					value += this[i][k] * cMatrix[k][j];
+				}
+				cDotMatrix[i][j] = value;
+			}
+		}
+		
+		return cDotMatrix;
+	}
+
+	CMatrix & CMatrix::operator*=(const CMatrix& cMatrix) override {
+		return (*this = *this * cMatrix);
 	}
 };
