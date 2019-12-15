98672f0bc77453235b2509f5f21df6a05ca466a9
KwonHyun <aptenodytes@live.co.kr>
2019-12-15 17:33:15 +0900

    Add Get Out Put

--- DeepLenaing/DeepLenaing/NeuralNetwork.hpp
+++ DeepLenaing/DeepLenaing/NeuralNetwork.hpp
@@ -5,15 +5,17 @@
 class CNeuralNetwork
 {
 public:
-	CNeuralNetwork( size_t input, size_t output, unsigned int nNumNode = 10, unsigned int nNumLayer = 3)
+	CNeuralNetwork( size_t inputSize, size_t outputSize, unsigned int nNumNode = 10, unsigned int nNumLayer = 3)
 		: m_nNumNode(nNumNode), m_nNumLayer(nNumLayer)
 	{
 		if (nNumLayer < 3)
 		{
-			throw std::out_of_range("Not Enough Layer");
+			std::ostringstream errStringStream;
+			errStringStream << nNumLayer << " < " << 3 << " Not Enough Layer ";
+			throw std::out_of_range(errStringStream.str());
 		}
 
-		CNode cInNode{ input, m_nNumNode };
+		CNode cInNode{ inputSize, m_nNumNode };
 		m_NodeVec.emplace_back(cInNode);
 
 		for (unsigned int i = 0; i < m_nNumLayer; i++)
@@ -22,9 +24,31 @@ public:
 			m_NodeVec.emplace_back(cTempNode);
 		}
 
-		CNode cOutNode{ m_nNumNode , output };
+		CNode cOutNode{ m_nNumNode , outputSize };
 		m_NodeVec.emplace_back(cOutNode);
 	}
+
+	std::valarray<double> GetOutPut(std::valarray<double> & input)
+	{
+		if (input.size() != m_NodeVec[0].size())
+		{
+			std::ostringstream errStringStream;
+			errStringStream << input.size() << " != " << m_NodeVec[0].size() << " Input is Not Same ";
+			throw std::out_of_range(errStringStream.str());
+		}
+
+		std::valarray<double> ret1;
+		auto ret2 = input;
+
+		for (auto it : m_NodeVec)
+		{
+			ret1 = it.GetOutPut(ret2);
+			std::swap(ret1, ret2);
+		}
+
+		return ret1;
+	}
+
 	~CNeuralNetwork()
 	{
 
--- DeepLenaing/DeepLenaing/Node.hpp
+++ DeepLenaing/DeepLenaing/Node.hpp
@@ -8,12 +8,12 @@ template<typename Ty = double>
 class CNode
 {
 public:
-	CNode(size_t width, size_t height)
+	CNode(size_t numCurNode, size_t numNextNode)
 	{
 		std::random_device rd{};
 		std::mt19937 gen{ rd() };
 
-		m_WeightList = std::make_shared<CMatrix<Ty>>( width , height );
+		m_WeightList = std::make_shared<CMatrix<Ty>>( numCurNode , numNextNode );
 
 		// 1 / √개수 로 초기화 한다.
 		std::normal_distribution<double> d{ 0, std::pow(m_WeightList->size(), -0.5) };
@@ -31,6 +31,32 @@ public:
 	}
 
 	~CNode() {}
+	
+	size_t size()
+	{
+		return m_WeightList[0].size();
+	}
+
+	std::valarray<Ty> GetOutPut( std::valarray<double>& input)
+	{
+		std::valarray<Ty> output(input.size());
+		if (m_WeightList[0].size() != input.size())
+		{
+			throw std::out_of_range("Matrix & Input Size different");
+		}
+
+		for (size_t i = 0; i < input.size(); i++)
+		{
+			double dVal = 0;
+			for (size_t j = 0; j < m_WeightList.size(); j++)
+			{
+				dVal += m_WeightList[i][j] * input[j];
+			}
+			output[i] = dVal;
+		}
+
+		return output;
+	}
 
 	CMatrix<Ty> && GetOutPut(CMatrix<Ty>& CMatrix)
 	{
